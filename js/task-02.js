c6a23375ca40528b9480ed2fc20d4a392247a035
JurekZela <122160360+JurekZela@users.noreply.github.com>
2023-06-10 10:22:44 +0200

    update hw2

--- js/task-02.js
+++ js/task-02.js
@@ -7,31 +7,13 @@ const ingredients = [
   'Condiments',
 ];
 
-const potatoesRef = document.createElement('li');
-potatoesRef.textContent = 'Potatoes';
-potatoesRef.classList.add('item');
+const ulRef = document.querySelector('ul');
 
-const mushroomsRef = document.createElement('li');
-mushroomsRef.textContent = 'Mushrooms';
-mushroomsRef.classList.add('item');
+ingredients.forEach((ingredient) => {
+  const li = document.createElement('li');
 
-const garlicRef = document.createElement('li');
-garlicRef.textContent = 'Garlic';
-garlicRef.classList.add('item');
+  li.textContent = ingredient;
+  li.classList.add('item');
 
-const tomatosRef = document.createElement('li');
-tomatosRef.textContent = 'Tomatos';
-tomatosRef.classList.add('item');
-
-const herbsRef = document.createElement('li');
-herbsRef.textContent = 'Herbs';
-herbsRef.classList.add('item');
-
-const condimentsRef = document.createElement('li');
-condimentsRef.textContent = 'Condiments';
-condimentsRef.classList.add('item');
-
-
-const ulRef = document.querySelector('#ingredients');
-ulRef.append(potatoesRef, mushroomsRef, garlicRef, tomatosRef, herbsRef, condimentsRef);
-console.log(ulRef);
+  ulRef.appendChild(li);
+});
